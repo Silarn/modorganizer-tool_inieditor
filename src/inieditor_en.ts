3d17ee617096652179c81804651ca5bcad07b8b6
Tannin <sherb@gmx.net>
2016-06-04 19:17:23 +0200

    translation

---
+++ src/inieditor_en.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>IniEditor</name>
+    <message>
+        <location filename="inieditor.cpp" line="57"/>
+        <source>Very basic editor for ini files</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="inieditor.cpp" line="86"/>
+        <source>INI Editor</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="inieditor.cpp" line="91"/>
+        <source>Edit the ini file of the current profile</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="inieditor.cpp" line="102"/>
+        <source>plugin not initialized</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="inieditor.cpp" line="127"/>
+        <source>Ini files are local to the currently selected profile.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="inieditor.cpp" line="136"/>
+        <source>File too big</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="inieditor.cpp" line="137"/>
+        <source>Sorry, the file %1 is too large to be handled by the Ini Editor</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
